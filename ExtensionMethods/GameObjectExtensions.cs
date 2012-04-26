2fad4299d1c4ecfc2d88409171d5af3ba4d05725
Jørgen P. Tjernø <jorgenpt@gmail.com>
2012-04-26 02:06:30 -0700

    Add GameObject#GetEncapsulatingBounds.
    GetEncapsulatingBounds returns the bounds of all contained renderers of the GO.

--- ExtensionMethods/GameObjectExtensions.cs
+++ ExtensionMethods/GameObjectExtensions.cs
@@ -13,6 +13,20 @@ namespace ExtensionMethods
 			}
 			return mask;
 		}
+
+		public static Bounds GetEncapsulatingBounds (this GameObject go)
+		{
+			Bounds bounds = new Bounds (Vector3.zero, Vector3.zero);
+			if (go.renderer != null)
+				bounds = go.renderer.bounds;
+
+			foreach (Renderer r in go.GetComponentsInChildren<Renderer> ())
+			{
+				bounds.Encapsulate (r.bounds);
+			}
+
+			return bounds;
+		}
 	}
 }
 
